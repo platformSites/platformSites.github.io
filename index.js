83c3a44df104e3d28a11521c2379675cb2686f33
Programmer <66716255+Maksim990@users.noreply.github.com>
2021-05-16 23:07:23 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+let on = true;
+let off = false;
+
+if(on){
+	console.log("%c ЧТОООООООООООООООООООООООООООООООООООООООООООООООООО, да ты по умнел, если хочешь помочь проекту или идеями, пишите создателю сайта, в дискорд сервер Котик#9821", "background: #222; color: #bada55");
+};
+
+localStorage.index_kc = "Compilater Codes";
